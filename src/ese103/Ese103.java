ce8a0d5ce0286b64011244da7b38e52dfd2daa9b
lcgiovannetti <lucagiovannetti1997@hotmail.it>
2015-03-20 17:16:41 +0100

    passo2

--- src/ese103/Ese103.java
+++ src/ese103/Ese103.java
@@ -1,9 +1,23 @@
+/**
+ * @author Luca Giovannetti
+ */
 package ese103;
+
+import geometricshapes.*;
 
 public class Ese103 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Circle c1 = new Circle(2,4,6);
+		Circle c2 = new Circle(2,4,6);
+		Circle c3 = new Circle(2,5,6);
+		Circle c4 = new Circle(2,4,8);
+
+		
+		System.out.println("C1 contiene C2? " +c1.contains(c2));
+		System.out.println("C2 contiene C1? " +c2.contains(c1));
+		System.out.println("C1 contiene C3? " +c1.contains(c3));
+		System.out.println("C1 contiene C4? " +c1.contains(c4));
 
 	}
 
---
+++ src/geometricshapes/Circle.java
@@ -0,0 +1,144 @@
+package geometricshapes;
+
+public class Circle {
+	
+	private double xC;
+	private double yC;
+	private double r;
+	
+	public Circle(double xC, double yC, double r) {
+		this.xC = xC;
+		this.yC = yC;
+		if(r>0){
+			this.r = r;
+		}
+		else{
+			throw new IllegalArgumentException("Il raggio deve essere maggiore di 0");
+		}
+		
+	}
+	/**
+	 * 
+	 * @return ascissa x del centro del cerchio
+	 */
+	public double getXC() {
+		return xC;
+	}
+	/**
+	 * setta la coordinata x del centro del cerchio
+	 * @param xC coordinata x del centro del cerchio
+	 */
+	public void setXC(double xC) {
+		this.xC = xC;
+	}
+	/**
+	 * 
+	 * @return ordinata y del centro del cerchio
+	 */
+	public double getYC() {
+		return yC;
+	}
+	/**
+	 * setta la coordinata y del centro del cerchio
+	 * @param yC coordinata y del centro del cerchio
+	 */
+	public void setYC(double yC) {
+		this.yC = yC;
+	}
+	/**
+	 * 
+	 * @return raggio del cerchio
+	 */
+	public double getR() {
+		return r;
+	}
+	/**
+	 * setta il raggio del cerchio, il quale deve essere maggiore di 0
+	 * @param r raggio del cerchio
+	 */
+	public void setR(double r) {
+		if (r>0){
+			this.r = r;
+		}
+		else
+			throw new IllegalArgumentException("Il raggio deve essere maggiore di 0");
+	}
+	/**
+	 * 
+	 * @return ascissa minore della bounding box del cerchio
+	 */
+	public double getXMin(){
+		return this.xC - this.r;
+	}
+	/**
+	 * 
+	 * @return ordinata minore della bounding box del cerchio
+	 */
+	public double getYMin(){
+		return this.yC - this.r;
+	}
+	/**
+	 * 
+	 * @return ascissa maggiore della bounding box del cerchio
+	 */
+	public double getXMax(){
+		return this.xC + this.r;
+	}
+	/**
+	 * 
+	 * @return ordinata maggiore della bounding box del cerchio
+	 */
+	public double getYMax(){
+		return this.yC + this.r;
+	}
+	
+	/**
+	 * Controlla se la bounding box di questo cerchio contiene quella di un altro cerchio
+	 * @param c
+	 * @return true se contiene la bounding box dell'altro cerchio, altrimenti false
+	 */
+	public boolean contains(Circle c){
+		boolean cont=false;
+		if(this.getXMin() <= c.getXMin() && this.getYMin() <= c.getYMin() && this.getXMax() >= c.getXMax() && this.getYMax() >= c.getYMax() ){
+			cont = true;
+		}
+		return cont;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(r);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(xC);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(yC);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Circle other = (Circle) obj;
+		if (Double.doubleToLongBits(r) != Double.doubleToLongBits(other.r))
+			return false;
+		if (Double.doubleToLongBits(xC) != Double.doubleToLongBits(other.xC))
+			return false;
+		if (Double.doubleToLongBits(yC) != Double.doubleToLongBits(other.yC))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "circle [xC=" + xC + ", yC=" + yC + ", r=" + r + "]";
+	}
+	
+
+}
